92fddb7a56fdaec8cd55d324101cfc2b41b25c88
Harsh Bhatia <90619168+hbhatia14@users.noreply.github.com>
2023-01-11 15:36:21 +0400

    Add files via upload

---
+++ my_first_code_in_python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this is my first line of code
+
+print("My name is Harsh Bhatia")
+print("The date today is 11th Jan 2023")
+
+
+# In[2]:
+
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+total_sales = a + b + c + d + e
+average_sales = (total_sales / 5)
+missing_sales = c - 6500
+
+print(total_sales)
+print(average_sales)
+print(missing_sales)
+
+
+# In[ ]:
+
+
+
+
